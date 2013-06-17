7222f06d7052de9a1e027f5b96831e36263bf5e9
Andrew Wirick <awirick@gmail.com>
2013-06-17 10:59:28 -0500

    initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# LinoType FontExplorer v1.2.3 Puppet Module for Boxen
+
+A Sketch app installer for LinoType FontExplorer
+
+## Usage
+
+```puppet
+include fontexplorer
+```
+
+## Required Puppet Modules
+
+* `boxen`
---
+++ manifests/init.pp
@@ -0,0 +1,6 @@
+class fontexplorer {
+  package { 'FontExplorer':
+    provider => 'appdmg',
+    source   => "http://download2us.softpedia.com/dl/c06dc0e114773bf8f4b3289c83b6c84f/51afa940/400010503/mac/Font-Tools/FontExplorerX123.dmg",
+  }
+}
